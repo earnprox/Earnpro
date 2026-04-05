997c835bac5bd0861e583c58023af49fd5c856c0
earnprox <earnpro1@gmail.com>
2026-04-05 19:36:37 +0530

    Update login-pin.js

--- api/login-pin.js
+++ api/login-pin.js
@@ -1,7 +1,7 @@
 const { db } = require('./_firebase');
 const crypto = require('crypto');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).end();
     
     const { phone, pinHash } = req.body;
@@ -24,4 +24,4 @@ export default async function handler(req, res) {
         console.error("Login error:", e);
         return res.status(500).json({ error: 'Server error' });
     }
-}
+};
