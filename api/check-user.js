689b595653b87fa5969fa073a4dcb826ce157995
earnprox <earnpro1@gmail.com>
2026-04-05 19:35:56 +0530

    Update check-user.js

--- api/check-user.js
+++ api/check-user.js
@@ -1,6 +1,6 @@
 const { db } = require('./_firebase');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).end();
     
     const { phone } = req.body;
@@ -17,4 +17,4 @@ export default async function handler(req, res) {
         console.error("Check user error:", e);
         return res.status(500).json({ error: 'Server error' });
     }
-}
+};
